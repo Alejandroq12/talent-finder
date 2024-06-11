0f8543928adc96c092f2dae2becc11029700d2a9
alejandroq12 <jaquezada@innovacion.gob.sv>
2024-06-11 15:24:37 -0600

    Chore: Get started with Vue.js-Tailwind instead of HTML, JavaScript and CSS

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue';
+import TheNavigationBar from '../components/TheNavigationBar.vue';
+</script>
+
+<template>
+  <main>
+    <TheNavigationBar />
+    <TheWelcome />
+  </main>
+</template>
