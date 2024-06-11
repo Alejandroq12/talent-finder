40014d0d14074ac36154b4cdb40d2a4520c310c8
alejandroq12 <jaquezada@innovacion.gob.sv>
2024-06-11 15:33:26 -0600

    Feat: Set up Tailwildcss and PrimeVue

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Tailwind from 'primevue/passthrough/tailwind'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(PrimeVue, {
+  unstyled: false,
+  pt: Tailwind
+})
 app.mount('#app')
